de21fc922a62f5d8f4af5083b596443e0e331295
NotSleepp <danielfranconot@gmail.com>
2022-12-06 13:58:05 -0300

    mejorando diseño y agregando contador carrito

--- src/components/cartWidget/CartWidget.jsx
+++ src/components/cartWidget/CartWidget.jsx
@@ -3,8 +3,10 @@ import './cartWidget.css'
 
 export const CartWidget = () => {
     return(
+        
         <div>
-           <a href='/#'><FaShoppingCart size={'3rem'} color={'#fff'} /></a> 
+           <a href='/#'><FaShoppingCart style={{position: "relative"}} size={'2rem'} color={'#e5e4e2'} /></a>
+            <p className='carrito-contador'>1</p> 
         </div>
     )
 } 
